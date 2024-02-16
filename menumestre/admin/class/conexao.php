44e725c4c7f80a89809a7f3fbb85a92aabf940ae
Kellinton <kellintonbrasil@gmail.com>
2024-02-15 22:05:58 -0300

    add

--- menumestre/admin/class/conexao.php
+++ menumestre/admin/class/conexao.php
@@ -6,7 +6,7 @@ class Conexao{
     public static function LigarConexao(){
         //PDO serve para fazer conexão com o banco de dados
         $conn = new PDO('mysql:dbname=dbmenumestre;host=localhost', 'root', '');
-        //$conn = new PDO('mysql:dbname=u283879542_webdequebrada;host=smpsistema.com.br', 'u283879542_webdequebrada', 'SenacAgencia01'); //banco hospedado no servidor
+       
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
 
